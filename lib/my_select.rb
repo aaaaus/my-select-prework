4f706fbe41cfb9f104896eff320c34b33368b90a
Austin Luft <ausluft@gmail.com>
2018-09-24 00:26:26 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,10 @@
 def my_select(collection)
-  if collection.length = 0 
-    return nil
-  else
   i = 0
+  new_collection = []
+  
   while i < collection.length
   if yield(collection[i]) == true 
-    
+    new_collection << collection[i]
   i = i + 1
   end
 end
