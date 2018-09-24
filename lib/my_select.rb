10d898fae361692bf591c1e074228e6ca774a9f2
Austin Luft <ausluft@gmail.com>
2018-09-24 00:27:04 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,10 +3,10 @@ def my_select(collection)
   new_collection = []
   
   while i < collection.length
-  if yield(collection[i]) == true 
-    new_collection << collection[i]
+    if yield(collection[i]) == true 
+      new_collection << collection[i]
+    end
   i = i + 1
   end
-end
-  collection
+  new_collection
 end
