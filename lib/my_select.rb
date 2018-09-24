cede390d31617355dd6d3ac7a57dec8d1c745363
Austin Luft <ausluft@gmail.com>
2018-09-24 00:19:21 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,12 +1,12 @@
 def my_select(collection)
-  if array.length = 0 
+  if collection.length = 0 
     return nil
   else
   i = 0
-  while i < array.length
-  yield(array[i])
+  while i < collection.length
+  yield(collection[i])
   i = i + 1
   end
 end
-  array
+  collection
 end
